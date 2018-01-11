4d7054e40493bbf5b84e78354beb1a7ab1138f6b
Markus Wolf <mail@markus-wolf.de>
2018-01-11 08:27:50 +0100

    chore: setup styled components

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,15 @@
 import { bind } from "decko";
 import { observer } from "mobx-react";
 import * as React from "react";
+import styled from "styled-components";
 import { external, inject } from "tsdi";
 
 import { CounterStore } from "./counter-store";
 import { DoubleCounter } from "./doubles-store";
+
+const AppContainer = styled.div`
+  line-height: 2em;
+`;
 
 @observer
 @external
@@ -15,12 +20,12 @@ export class App extends React.Component {
 
   public render(): JSX.Element {
     return (
-      <div>
+      <AppContainer>
         <h1>Hello World</h1>
         <p>Clicked {this.store.counter} times</p>
         <p>Doubled it is {this.doubler.doubled} times</p>
         <button onClick={this.onClick}>Hit me!</button>
-      </div>
+      </AppContainer>
     );
   }
 
